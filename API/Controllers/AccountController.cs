f7bd7b44f7c5e0006538ce5096f26514fe6d1562
Naser Abdullah Alam <iamnaser1998@gmail.com>
2023-07-24 11:40:31 +0600

    Added- LoginEndpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using API.DTOs.Account;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jWTService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(JWTService jwtService, 
+            SignInManager<User> signInManager, 
+            UserManager<User> userManager)
+        {
+            _jWTService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null) return Unauthorized("Invalid username or password");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded) return Unauthorized("Invalid username or password");
+
+            return CreateApplicationUserDto(user);
+        }
+
+
+        #region Helper Methods
+        private UserDto CreateApplicationUserDto(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                JWT = _jWTService.CreateJWT(user),
+            };
+        }
+        #endregion
+    }
+}

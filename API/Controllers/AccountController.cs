2c5308f01e2c080c9b876ef51c8742d9fc1a41de
Naser Abdullah Alam <iamnaser1998@gmail.com>
2023-07-24 21:17:04 +0600

    Added- RegisterRequest

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
 ﻿using API.DTOs.Account;
 using API.Models;
 using API.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,8 +15,8 @@ namespace API.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
 
-        public AccountController(JWTService jwtService, 
-            SignInManager<User> signInManager, 
+        public AccountController(JWTService jwtService,
+            SignInManager<User> signInManager,
             UserManager<User> userManager)
         {
             _jWTService = jwtService;
@@ -25,17 +25,38 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.FindByNameAsync(model.UserName);
+            var user = await _userManager.FindByNameAsync(loginDto.UserName);
             if (user == null) return Unauthorized("Invalid username or password");
 
-            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
             if (!result.Succeeded) return Unauthorized("Invalid username or password");
 
             return CreateApplicationUserDto(user);
         }
 
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (await CheckEmailExistsAsync(registerDto.Email))
+                return BadRequest($"{registerDto.Email} already Exists!");
+
+            var userToAdd = new User
+            {
+                FirstName = registerDto.FirstName.ToLower(),
+                LastName = registerDto.LastName.ToLower(),
+                UserName = registerDto.Email.ToLower(),
+                Email = registerDto.Email.ToLower(),
+                EmailConfirmed = true
+
+            };
+
+            var result = await _userManager.CreateAsync(userToAdd, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            return Ok("Account created successfully");
+        }
 
         #region Helper Methods
         private UserDto CreateApplicationUserDto(User user)
@@ -46,6 +67,10 @@ namespace API.Controllers
                 LastName = user.LastName,
                 JWT = _jWTService.CreateJWT(user),
             };
+        }
+        private async Task<bool> CheckEmailExistsAsync(string email)
+        {
+            return await _userManager.Users.AnyAsync(x => x.Email == email.ToLower());
         }
         #endregion
     }

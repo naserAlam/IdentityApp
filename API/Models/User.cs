19795b04b9a490640133e6113432323569cbad9f
Naser Abdullah Alam <iamnaser1998@gmail.com>
2023-07-24 22:50:40 +0600

    Update- AddedCode

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -9,6 +9,8 @@ namespace API.Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [Required]
+        public int Code { get; set; }
         public DateTime CreationTime { get; set; } = DateTime.UtcNow;
     }
 }
